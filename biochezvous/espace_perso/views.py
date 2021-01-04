aeb4333bc6bce9d3b4b30557128b98866199a2c6
BaptisteAlix <alix.baptiste14@gmail.com>
2021-01-04 15:26:31 +0100

    Premier commit: creation du projet et d'une application espace_perso

--- biochezvous/espace_perso/views.py
+++ biochezvous/espace_perso/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.db import connection
+from collections import namedtuple
 # Create your views here.
+
+
+
+def my_custom_sql():
+    
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM bcv._personne")
+        table = namedtuplefetchall(cursor)
+        #table = cursor.fetchall()
+    return table
+
+def namedtuplefetchall(cursor):
+    "Return all rows from a cursor as a namedtuple"
+    desc = cursor.description
+    nt_result = namedtuple('Result', [col[0] for col in desc])
+    return [nt_result(*row) for row in cursor.fetchall()]
+
+def index(request):
+    texte = "hello"
+    texte += "<ul>"
+    for i in my_custom_sql():
+        texte+= "<li>"
+        for j in i:
+            texte+= " - "+str(j)
+        texte += "</li>\n"
+    texte += "</ul>"
+    return HttpResponse(texte)
