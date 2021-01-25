5c302cd8222e85942f7ddedaef48b7408b7fb990
ghost-hikaru <72512491+ghost-hikaru@users.noreply.github.com>
2021-01-25 15:03:58 +0100

    modif inscription

--- biochezvous/espace_perso/urls.py
+++ biochezvous/espace_perso/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.wip_userlist, name='wip_userlist'),
+    #path('', views.wip_userlist, name='wip_userlist'),
     path('',views.wip_connexion, name='wip_connexion'),
     path('',views.wip_inscription, name='wip_inscription'),
 ]
