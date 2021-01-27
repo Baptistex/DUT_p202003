a4b3c612fa55ad5227654039ae152e8fe44b2fed
BaptisteAlix <alix.baptiste14@gmail.com>
2021-01-27 13:33:38 +0100

    debut de la page d'ajout de produit, des tables Produit et TypeProduit

--- biochezvous/produit/models.py
+++ biochezvous/produit/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class TypeProduit(models.Model):
+    type_id = models.AutoField(primary_key=True)
+    nom = models.TextField()
+    info = models.TextField()
+    prix = models.FloatField()
+    tva = models.FloatField()
+    unit = models.BooleanField()
+    def __str__(self):
+        return "TypeProduit : "+str(type_id)
+    
+    class Meta:
+        db_table = 'typeproduit'
+
+
+class Produit(models.Model):
+    produit_id = models.AutoField(primary_key=True)
+    type_id = models.ForeignKey('TypeProduit', on_delete=models.CASCADE)
+    date = models.DateField()
+    quantite = models.FloatField()
+    unite = models.IntegerField()
+
+    def __str__(self):
+        return "Produit : "+str(self.produit_id)+str(type_id)
+    
+    class Meta:
+        db_table = 'produit'
--- biochezvous/produit/views.py
+++ biochezvous/produit/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from produit.models import TypeProduit, Produit
+
 
 # Create your views here.
 
@@ -17,5 +19,7 @@ def liste_produit(request):
 
 
 def ajout_prod(request):
-    template = loader.get_template('produit/produits.html')
+    template = loader.get_template('produit/ajout_produit.html')
+    produit_exemple = TypeProduit(nom = "Patate", info = "Pomme de terre rouge", prix = 2.3, tva = 0.20, unit = False)
+    produit_exemple.save()
     return HttpResponse(template.render({},request))
