ad561bc8a99c38f63a9facce69f955ff5b8525ef
pablo29800 <paulobreton29800@gmail.com>
2021-02-01 12:17:37 +0100

    update main.js

--- biochezvous/staticfiles/js/main.js
+++ biochezvous/staticfiles/js/main.js
@@ -109,7 +109,7 @@
 
   // Toggle .header-scrolled class to #header when page is scrolled
   $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
+    if ($(this).scrollTop() > 0) { // valeur de scroll pour savoir quand afficher le fond de color #555555 de la barre de nav
       $('#header').addClass('header-scrolled');
     } else {
       $('#header').removeClass('header-scrolled');
