b169a35b0b1247585f6aef49ee943063cdc5683d
ghost-hikaru <72512491+ghost-hikaru@users.noreply.github.com>
2021-02-06 12:07:30 +0100

    inscription d'un utilisateur operationnelle

--- biochezvous/espace_perso/models.py
+++ biochezvous/espace_perso/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Personne(models.Model):
-    person_id = models.AutoField(primary_key=True)
+    id_personne = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
     mot_de_passe = models.CharField(max_length=100)
