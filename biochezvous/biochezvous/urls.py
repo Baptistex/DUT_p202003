d45a4576d623500c23147646de890dec728c301e
BaptisteAlix <alix.baptiste14@gmail.com>
2021-02-08 14:15:21 +0100

    Correctif suppression espace_admin

--- biochezvous/biochezvous/urls.py
+++ biochezvous/biochezvous/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
     path('', include('espace_perso.urls')),
     path('', include('produit.urls')   ),
     path('', include('accueil.urls')   ),
-    path('', include('espace_admin.urls')),
     path('admin/', admin.site.urls),
 ]
 
