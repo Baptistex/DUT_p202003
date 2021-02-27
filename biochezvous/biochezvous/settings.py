506feb955ad0547b687132dfc6c6736b311fcc10
tlaharotte <thomas.laharotte@etudiant.univ-rennes1.fr>
2021-02-27 14:53:46 +0100

    Envoie automatique de mail
    Premier test de fonction d'envois de mails

--- biochezvous/biochezvous/settings.py
+++ biochezvous/biochezvous/settings.py
@@ -145,9 +145,9 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 # Django Emails
 
 
-
-EMAIL_HOST = ''
-EMAIL_PORT = ''
-EMAIL_HOST_USER = ''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'biochezvous.iut@gmail.com'
 EMAIL_HOST_PASSWORD = 'Ventes!?loc2021'
 EMAIL_USE_TLS = True
--- biochezvous/espace_perso/utils.py
+++ biochezvous/espace_perso/utils.py
@@ -1,11 +1,13 @@
 from django.core.mail import send_mail
 from .models import Utilisateur,Personne,Producteur
+from django.http import request
 
 def send_mail_inscription():
-    send_mail(
-    'Bienvenue sur BioChezVous',
-    'Bienvenue' ['nom'],
-    'biochezvous.iut@gmail.com',
-    ['mail'],
-    fail_silently=False,
-    )
+    if(request.GET.get('pay')):
+        send_mail(
+        'Bienvenue sur BioChezVous',
+        'Bienvenue' ['nom'],
+        'biochezvous.iut@gmail.com',
+        ['thomas.laharotte@gmail.com'],
+        fail_silently=False,
+        )
