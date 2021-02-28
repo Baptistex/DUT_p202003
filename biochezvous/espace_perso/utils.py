1e9e9fe08631cada5763e23a0b1f365aa773f76c
tlaharotte <thomas.laharotte@etudiant.univ-rennes1.fr>
2021-02-28 18:05:59 +0100

    Envois de mail de masse
    Début de la fonction

--- biochezvous/espace_perso/utils.py
+++ biochezvous/espace_perso/utils.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from .models import Utilisateur,Personne,Producteur
 from django.http import request
+from django.core.mail import EmailMessage
 
 def send_mail_inscription():
     send_mail(
@@ -10,3 +11,14 @@ def send_mail_inscription():
     ['thomas.laharotte@gmail.com'],
     fail_silently=False,
     )
+
+
+
+def mail_mass():
+    EmailMessage(
+    'Hello',
+    'Body goes here',
+    'biochezvous.iut@gmail.com',
+    [''],
+    ['bcc@example.com'],
+)
