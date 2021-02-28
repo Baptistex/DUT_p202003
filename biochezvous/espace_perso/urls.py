bd92840c3a5fc6988b318f15df3b4ad574f78352
tlaharotte <thomas.laharotte@etudiant.univ-rennes1.fr>
2021-02-28 17:08:13 +0100

    Envois de mails auto
    Fonction ok il faut encore la lié aux pages html concerné

--- biochezvous/espace_perso/urls.py
+++ biochezvous/espace_perso/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import views as auth_views
 from . import views
+from . import utils
 
 urlpatterns = [
     path('userlist', views.wip_userlist, name='wip_userlist'),
@@ -12,6 +13,7 @@ urlpatterns = [
     path('connexion', views.connexion, name='connexion'),
     path('deconnexion', views.deconnexion, name='deconnexion'),
     path('connexion', auth_views.LoginView.as_view()),
+    path('paiement', utils.send_mail_inscription, name='send_mail_inscription')
 
     
     
--- biochezvous/espace_perso/utils.py
+++ biochezvous/espace_perso/utils.py
@@ -3,11 +3,10 @@ from .models import Utilisateur,Personne,Producteur
 from django.http import request
 
 def send_mail_inscription():
-    if(request.GET.get('pay')):
-        send_mail(
-        'Bienvenue sur BioChezVous',
-        'Bienvenue' ['nom'],
-        'biochezvous.iut@gmail.com',
-        ['thomas.laharotte@gmail.com'],
-        fail_silently=False,
-        )
+    send_mail(
+    'Bienvenue sur BioChezVous',
+    'Bienvenue' ['nom'],
+    'biochezvous.iut@gmail.com',
+    ['thomas.laharotte@gmail.com'],
+    fail_silently=False,
+    )
