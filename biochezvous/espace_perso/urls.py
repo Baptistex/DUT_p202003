2cdd3383a6c6dd850839a4f0ab4cf3d75a672f4a
BaptisteAlix <alix.baptiste14@gmail.com>
2021-04-05 17:52:19 +0200

    Unification espacePerso + nettoyage URLs
    Les noms d'URL sont plus propre
    
    EspacePerso et espaceProducteur sont  accessible à la même url '/profil'

--- biochezvous/espace_perso/urls.py
+++ biochezvous/espace_perso/urls.py
@@ -4,34 +4,34 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('paiement',views.paiement, name='paiement'),
-    path('espacePerso',views.espacePerso, name='espacePersoUser'),
-    path('inscription',views.inscription_user, name='inscription_user'),
-    path('inscription_prod',views.inscription_prod, name='inscription_prod'),
-    path('connexion', views.connexion, name='connexion'),
-    path('deconnexion', views.deconnexion, name='deconnexion'),
-    path('connexion', auth_views.LoginView.as_view()),
-    path('aide', views.aide, name='aide'),
-    path('personne/', views.delete_user, name='delete_user'),
-    path('personne/<int:id>/', views.deleteOneUser, name='deleteOneUser'),
-    path('accueilEspaceProducteur', views.espace_producteur, name='espace_producteur'),
-    path('espaceProducteur',views.espacePersoProd, name='espacePersoProd'),
-    path('listeCommande',views.listeCommande, name='listeCommande'),
-    path('commande/<int:id>',views.commande, name='commande'),
-    path('informationPerso',views.informationPerso, name='informationsPerso'),
-    path('panier',views.panier, name='panier'),
-    path('suppressionPanier/<int:id>',views.suppressionPanier, name='suppressionPanier'),
-    path('varierArticlePanier',views.varierArticlePanier, name='varierArticlePanier'),
-    path('nouvelleAdresse',views.ajout_prod_adresse, name='ajout_prod_adresse'),
-    path('commander',views.commander, name='commander'),
-    path('espace_Personnelle',views.redirection,name='rediriger_utilisateur'),
-    
-    path('commandeProducteur',views.commandeProducteur, name='commandeProducteur'),
-   
-    #espace producteur 
-    path('producteur/<int:idProducteur>', views.producteur, name='producteur'),
-    path('test', views.index, name="test"),
-    path('pdf_view/<int:id>', views.PDF, name="pdf_view"),
-    path('pdf_download/', views.DownloadPDF.as_view(), name="pdf_download"),
+    #Connexions/Inscriptions
+    path('inscription',                     views.inscription_user,     name='inscription_user'),
+    path('inscription/producteur',          views.inscription_prod,     name='inscription_prod'),
+    path('connexion',                       views.connexion,            name='connexion'),
+    path('connexion',                       auth_views.LoginView.as_view()),
+    path('deconnexion',                     views.deconnexion,          name='deconnexion'),
+    #Utilisateur        
+    path('profil',                          views.espacePerso,          name='espacePerso'),
+    path('profil/edit',                     views.informationPerso,     name='informationPerso'),
+    path('profil/adresse/edit',             views.ajout_prod_adresse,   name='ajout_prod_adresse'),
+    #Producteur
+    path('profil/producteur/commandes',     views.commandeProducteur,   name='commandeProducteur'),
+    #Panier/Commande
+    path('profil/panier',                   views.panier,               name='panier'),
+    path('profil/panier/<int:id>/suppression',views.suppressionPanier,  name='suppressionPanier'),
+    path('profil/panier/commander',         views.commander,            name='commander'),
+    path('profil/panier/paiement',          views.paiement,             name='paiement'),
+    path('profil/commandes',                views.listeCommande,        name='listecommande'),
+    path('profil/commande/<int:id>',        views.commande,             name='commande'),
+    path('profil/panier/varier',            views.varierArticlePanier,  name='varierArticlePanier'),
+    #Catalogue
+    path('producteur/<int:idProducteur>',   views.producteur,           name='producteur'),
+    #Administration
+    path('personne/<int:id>/supprimer',     views.deleteOneUser,     name='deleteOneUser'),
+    path('aide',                    views.aide,                 name='aide'),
+    #Factures
+    path('test',                    views.index,                name="test"),
+    path('pdf_view/<int:id>',       views.PDF,                  name="pdf_view"),
+    path('pdf_download/',           views.DownloadPDF.as_view(),name="pdf_download"),
     
 ]
