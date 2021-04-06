04b62f9d15ec1a262e26fbb87c0a815c68722d7a
BaptisteAlix <alix.baptiste14@gmail.com>
2021-04-06 13:25:03 +0200

    ajout dans le path de l'url vide

--- biochezvous/accueil/urls.py
+++ biochezvous/accueil/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('accueil', views.accueil, name='accueil'),
+    path('', views.accueil, name='accueil2'),
+
     path('propos', views.propos, name='propos'),
 
 ]
